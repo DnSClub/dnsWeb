537793c7a2c5cb1376ce57c0ffed73188851e1c2
sophiadt <66497192+sophiadt@users.noreply.github.com>
2023-09-04 19:10:15 -0700

    fix bg

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,16 +1,7 @@
 import { Link } from "react-router-dom";
-
 import classes from "./MainNavigation.module.css";
-import { useEffect } from "react";
 
 function MainNavigation() {
-  useEffect(() => {
-    document.body.style.background =
-      "linear-gradient(120deg, #020091, #A2004C, #FF014A)";
-    document.body.style.backgroundSize = "100% 1100%";
-    document.body.style.animation = "gradientAnimation 30s ease infinite";
-  }, []);
-
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,35 +1,7 @@
-import { useEffect } from "react";
 import classes from "./Projects.module.css";
 import { AiFillGithub } from "react-icons/ai";
 
 function ProjectsPage() {
-  useEffect(() => {
-    document.body.style.background =
-      "linear-gradient(120deg, #000243, #420064, #020091, #A2004C, #420064)";
-    document.body.style.backgroundSize = "600% 600%";
-    document.body.style.animation = "gradientAnimation 15s ease infinite";
-    // Define the @keyframes rule for the gradientAnimation
-    const styleSheet = document.styleSheets[0];
-    if (styleSheet) {
-      styleSheet.insertRule(
-        `
-        @keyframes gradientAnimation {
-          0% {
-            background-position: 0% 50%;
-          }
-          50% {
-            background-position: 100% 50%;
-          }
-          100% {
-            background-position: 0% 50%;
-          }
-        }
-      `,
-        styleSheet.cssRules.length
-      );
-    }
-  }, []);
-
   return (
     <div className={classes.container}>
       <div className={classes.mainTitle}>
